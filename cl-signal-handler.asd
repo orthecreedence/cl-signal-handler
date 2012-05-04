eb6723845d204c9137765df61a1493fdea2ab646
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-05-03 21:16:19 -0700

    initial;

---
+++ cl-signal-handler.asd
@@ -0,0 +1,6 @@
+(asdf:defsystem :cl-signal-handler
+  :author "Andrew Lyon <orthecreedence@gmail.com>"
+  :licence "MIT"
+  :version "0.1.0"
+  :depends-on (:cffi)
+  :components ((:file "signal")))
---
+++ signal.lisp
@@ -0,0 +1,44 @@
+;;; This is a simple library for creating signal handlers for unix signals in
+;;; Common Lisp. Its only dependency is CFFI, and it is only tested on *NIX
+;;; systems. Chances are, windows won't work ='[.
+;;;
+;;; Any implmentation that supports CFFI and is running on a posix system
+;;; should be able to use this library.
+;;;
+;;; Although I'm no expert in C and cannot recommend best practices for this
+;;; library (sorry!!), I can point you to an excellent resource:
+;;;
+;;;   http://www.cs.utah.edu/dept/old/texinfo/glibc-manual-0.02/library_21.html
+;;;
+;;; See full documentation at
+;;;
+;;;   https://github.com/orthecreedence/cl-signal-handler
+;;;
+;;; Please enjoy irresponsibly =].
+;;;
+;;; Andrew Lyon <orthecreedence@gmail.com>
+
+(defpackage :cl-signal-handler
+  (:export :set-signal-handler
+           :with-signal-handler)
+  (:nicknames :ush))
+(in-package :cl-signal-handler)
+
+(defmacro set-signal-handler (signo &body body)
+  "Replace the current handler for the signal number under signo, and return a
+  pointer to the handler that is being replaced."
+  (let ((handler (gensym "HANDLER")))
+    `(progn
+       (cffi:defcallback ,handler :void ((signo :int))
+         (declare (ignore signo))
+         ,@body)
+       (cffi:foreign-funcall "signal" :int ,signo :pointer (cffi:callback ,handler) :pointer))))
+
+(defmacro with-signal-handler (signo handler-fn &body body)
+  "Allows you to execute a block of code using the signal handling function
+  specified by handler-fn. Once the body is finished, the original signal
+  handler will always be restored."
+  `(let ((default-handler (set-signal-handler ,signo (funcall ,handler-fn))))
+     (unwind-protect 
+       (progn ,@body)
+       (cffi:foreign-funcall "signal" :int ,signo :pointer default-handler :pointer))))
